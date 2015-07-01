f7b47b5122f011faa806b83eb042f06fc0faee77
cqf1991 <cqf1324@vip.qq.com>
2015-07-01 17:09:08 +0800

    Calc test about operator

---
+++ calc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+a=int(raw_input('please input a'))
+b=int(raw_input('please input b'))
+
+print a+b
+print a-b
+print a*b
+print a/b
+print ~a
+print a>b and a>50
+print 3.0//2
+print 3.0/2
+print id(a)
+print 1<<2
+print 2**3**2
