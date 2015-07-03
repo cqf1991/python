3753ddc47ac9f3d25b60806a730b13e7de0dce60
cqf1991 <cqf1324@vip.qq.com>
2015-07-03 11:39:31 +0800

    elemType & string basic(index & slice)

---
+++ elemType.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+num1=123
+num2=123L
+num3=123.123
+num4=111111111111111111111111111111111111
+num5="132"
+num6=123j
+print type(num1)
+print type(num2)
+print type(num3)
+print type(num4)
+print type(num5)
+print type(num6)
+
+mail=""" abc:
+		asdf
+	sdfsd    sdfklj
+"""
+
+print mail
+a='abcde'
+print a[0]
+print a[0]+a[2]
+print a[1:4]
+print a[:4]
+print a[4:]
+print a[::2]
+print a[::-1]
+print a[-1]
+print a[-4:-1]
+print a[-2:-4:-1]
